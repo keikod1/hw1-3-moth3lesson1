2adb5e09f30bb62c47fe43897cb047e1ff69f760
keikod1 <madragimova@icloud.com>
2022-01-07 19:16:06 +0600

    Initial Commit

---
+++ hw1(3) moth3 lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1(3) moth3 lesson1
+//
+//  Created by my macbook on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
